84503e051388536b0603cd27bf9140a8248e84d0
aditya-debugs <adityaofficial1641@gmail.com>
2024-08-05 22:27:51 +0530

    added menu bar in sm devices

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,28 @@
+"use client";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <nav className=" flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul className="hidden h-full gap-12 lg:flex text-2xl">
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
             key={link.key}
-            className="regualar-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            className="regualar-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:text-green-50"
           >
             {link.label}
           </Link>
@@ -30,14 +37,29 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
       </div>
+      <div onClick={handleNav} className="block sm:hidden z-10">
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
 
-      <Image
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer lg:hidden"
-      />
+      <div
+        className={
+          nav
+            ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300"
+            : " sm:hidden absolute top-0 left-[100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300"
+        }
+      >
+        <ul className="text-4xl p-4 ">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regualar-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:text-green-50"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
